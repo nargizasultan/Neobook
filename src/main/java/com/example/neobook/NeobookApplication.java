b74b6aba48d20c413bff3ba0dbed4b115fa305c1
User <sultanmuratova500@gmail.com>
2023-12-16 20:34:44 +0300

    bnkj

---
+++ src/main/java/com/example/neobook/NeobookApplication.java
@@ -0,0 +1,13 @@
+package com.example.neobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeobookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeobookApplication.class, args);
+    }
+
+}
